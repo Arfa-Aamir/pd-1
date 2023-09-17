ec43f429024e7369ed36bb43af896f37b03f0b13
Arfa-Aamir <144796293+Arfa-Aamir@users.noreply.github.com>
2023-09-17 01:10:02 -0700

    Add files via upload

---
+++ gun.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+main(){
+
+cout<<"+--^-------------_-------_-------_-----------^-' "<<endl;
+cout<<" | ||||||     '--------'         |           '  0 "<<endl;
+cout<<" '+-------------------------------^---------------| "<<endl;
+cout<<"   '-----------'---------'----------------------'    "<<endl;
+cout<<"    '           '         '                           "<<endl;
+cout<<"    / XXXXXXX   /'|         /'                          "<<endl;
+cout<<"   / XXXXXXXX  /  |        /'                            "<<endl;
+cout<<"  / XXXXXXXXX /'--------'                               "<<endl;
+cout<<" / XXXXXXXXX /                                          "<<endl;
+cout<<"/ XXXXXXXXXX/                                           "<<endl;
+cout<<"(___________(                                            "<<endl;
+cout<<" '--------'                                              "<<endl;
+}
